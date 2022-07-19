da26137b8b879f140458f67eb50c2f6a37e17df5
Raaid Rizvi <raaidrizvi123@gmail.com>
2022-07-19 12:45:25 +0530

    Organizing Directory Structure

--- Design Patterns/Singleton/src/Application.java
+++ Design Patterns/Singleton/src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main (String[] args){
         String song = "Despacito";
-        
+
         AudioPlayer audioPlayer = AudioPlayer.getInstance();
         audioPlayer.playMusic(song);
     }
